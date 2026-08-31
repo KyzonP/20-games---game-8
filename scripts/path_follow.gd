29d21cac04bde1f9290695010153ddf8b2a249bf
KyzonP <JasonReed2772@gmail.com>
2026-08-31 14:52:32 +0100

    Movement reworked, speed change added

--- scripts/path_follow.gd
+++ scripts/path_follow.gd
@@ -1,6 +1,21 @@
 extends PathFollow3D
 
-@export var speed : float = 10.0
+@export var maxSpeed : float = 30.0
+@export var baseSpeed : float = 20.0
+@export var minSpeed : float = 10.0
+
+@export var acceleration : float = 10.0
+@export var deceleration : float = 10.0
+
+var speed : float = 10.0
 
 func _physics_process(delta):
+	if Input.is_action_pressed("boost"):
+		speed = move_toward(speed, maxSpeed, delta * acceleration)
+	elif Input.is_action_pressed("brake"):
+		speed = move_toward(speed, minSpeed, delta * deceleration)
+	else:
+		speed = move_toward(speed, baseSpeed, delta * deceleration)
+
 	progress += delta * speed
+	
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,19 +1,46 @@
-extends CharacterBody3D
+extends Area3D
 
-@export var speed : float = 10.0
-@export var margin : float = 0.5
+@export var max_speed : float = 10.0
+@export var acceleration : float = 10.0
+@export var deceleration : float = 20.0
+var x_speed : float = 0.0
+var y_speed : float = 0.0
+var speed
+@export var x_margin : float = 13.0
+@export var y_margin : float = 7.0
 
 @onready var camera : Camera3D = get_parent()
 
 func _physics_process(delta) -> void:
+	# Get input direction
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var move_dir = Vector3(input_dir.x, -input_dir.y, 0.0)
 	
-	velocity = move_dir * speed
-	move_and_slide()
+	if input_dir[0] < 0:
+		x_speed = move_toward(x_speed, -max_speed, delta * acceleration)
+	elif input_dir[0] > 0:
+		x_speed = move_toward(x_speed, max_speed, delta * acceleration)
+	else:
+		x_speed = move_toward(x_speed, 0.0, delta * deceleration)
+		
+	if input_dir[1] < 0:
+		y_speed = move_toward(y_speed, max_speed, delta * acceleration)
+	elif input_dir[1] > 0:
+		y_speed = move_toward(y_speed, -max_speed, delta * acceleration)
+	else:
+		y_speed = move_toward(y_speed, 0.0, delta * deceleration)
+		
+	position += Vector3(x_speed, y_speed, 0.0) * delta
 	
 	if camera:
 		lock_to_camera()
 		
 func lock_to_camera() -> void:
-	pass
+	if position.x > x_margin:
+		position.x = x_margin
+	elif position.x < -x_margin:
+		position.x = -x_margin
+	
+	if position.y > y_margin:
+		position.y = y_margin
+	elif position.y < -y_margin:
+		position.y = -y_margin
